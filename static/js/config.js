160f6a258be8fc1808e535a7ed6f9578203eb6e2
Жуков Александр <zhukov.msu@gmail.com>
2026-07-08 16:56:59 +0300

    some visual

--- static/js/config.js
+++ static/js/config.js
@@ -46,10 +46,10 @@ export const GAME = {
     { file: "char6.png", aspect: 0.78 }, // самокат
     { file: "char7.png", aspect: 1.28, scale: 1.5 }, // конная полиция — крупнее
     { file: "char8.png", aspect: 1.13 }, // загорающий с мопсом
-    { file: "char9.png", aspect: 0.93, scale: 1.15 }, // повар с пловом — чуть крупнее
+    { file: "char9.png", aspect: 0.93, scale: 1.3 }, // повар с пловом — чуть крупнее
     { file: "char10.png", aspect: 2.25, scale: 2.5 }, // газель с арбузами — крупная
     { file: "char11.png", aspect: 2.23, scale: 2.5 }, // пляжные зонты + афиша — крупная
-    { file: "char12.png", aspect: 1.00, scale: 1.3 }, // бочка кваса
+    { file: "char12.png", aspect: 1.00, scale: 1.35 }, // бочка кваса
     { file: "char13.png", aspect: 1.05, scale: 1.6 }, // знак «купаться запрещено» — крупный
     { file: "char14.png", aspect: 1.68 }, // мангал с шашлыком
     { file: "char15.png", aspect: 0.76, scale: 1.8 }, // пляжная кабинка — крупная
